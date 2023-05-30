f0095f8a52f7ff6b8204966f0a8bde46565750de
Adam <62599990+Adam-M-04@users.noreply.github.com>
2023-05-30 19:27:40 +0200

    Responsiveness improved

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'ex_lg': '1112px',
+      }
+    },
   },
   plugins: [],
 }
